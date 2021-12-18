ad8914e458fc474722aba17689fc624e171e00ba
Kurtis Parsons <kuparsons24@gmail.com>
2021-12-18 09:40:45 -0500

    updated homepage

--- src/Components/pages/HomePage.js
+++ src/Components/pages/HomePage.js
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import { Paper, Toolbar } from '@mui/material';
+import { forwardRef } from 'react';
+import { Button, Container, Grid, Paper, Stack, Toolbar, Typography } from '@mui/material';
 import ResponsiveAppBar from '../app_bar/ResponsiveAppBar';
 import { useTheme } from '@mui/system';
 
-export default function HomePage (props) {
+const HomePage = forwardRef((props, ref) => {
   const theme = useTheme();
 
   return(
-    <Paper square sx={{
+    <Paper ref={ref} square sx={{
       backgroundImage: `url(${props.backgroundImage})`,
       backgroundColor: 'darkslategray',
       backgroundBlendMode: theme.palette.mode === 'dark' ? 'multiply' : 'screen',
@@ -18,7 +19,24 @@ export default function HomePage (props) {
       height: '100vh',
     }}>
       {/* <Toolbar /> */}
-      {props.children}
+      <Container maxWidth='xl' sx={{display: 'flex', alignItems: 'center', height: '100vh'}}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant='h3' textAlign='center'>Kurtis Parsons - Full Stack Web Developer</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Stack spacing={3} direction='row' justifyContent='center'>
+              <Button size='large' variant='outlined'>Resume</Button>
+              <Button size='large' variant='outlined'>Projects</Button>
+            </Stack>
+          </Grid>
+          {/* <Grid container item xs={6} justifyContent='left'>
+
+          </Grid> */}
+        </Grid>
+      </Container>
     </Paper>
   );
-};
+});
+
+export default HomePage;
