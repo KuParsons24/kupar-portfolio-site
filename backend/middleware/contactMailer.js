18346b58fe5affbadeb1222ac24bde9b7d5ed6e3
Kurtis Parsons <kuparsons24@gmail.com>
2021-12-22 16:24:49 -0500

    made contact mailing middleware

---
+++ backend/middleware/contactMailer.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com", //replace with your email provider
+  port: 587,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
+const contactMailer = (req, res, next) => {
+  console.log(req.body.name);
+
+  const mail = {
+    from: req.body.name,
+    to: process.env.EMAIL,
+    subject: `Contact Submission from ${req.body.name}`,
+    text: `${req.body.name} <${req.body.email}> \n${req.body.message}`,
+  };
+  
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      console.log(err);
+      err.statusCode = 500;
+      next(err);
+    } else {
+      next();
+    }
+  });
+}
+
+module.exports = contactMailer;
