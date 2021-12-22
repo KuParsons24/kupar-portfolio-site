ea560c76ac0ab324e7f586068e60a84c2ac8eccd
Kurtis Parsons <kuparsons24@gmail.com>
2021-12-22 16:42:48 -0500

    created middleware for admin tool

--- backend/middleware/contactDb.js
+++ backend/middleware/contactDb.js
@@ -1,6 +1,6 @@
 const { Message } = require('../db/index').models;
 
-const createEntry = async (req, res, next) => {
+const handleFormSubmission = async (req, res, next) => {
   try {
     await Message.create({
       name: req.body.name,
@@ -14,6 +14,75 @@ const createEntry = async (req, res, next) => {
   next();
 };
 
+const getAllEntries = async (req, res, next) => {
+  try {
+    let messages = await Message.findAll({
+      attributes: ['id', 'name', 'email', 'message'],
+      order: [['id', 'DESC']]
+    });
+    res.json(messages);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+const deleteEntries = async (req, res, next) => {
+  try {
+    let message = {};
+    const obj = {
+      deleteSuccess: true
+    };
+    req.body.map(async (id) => {
+      message = await Message.destroy({
+        where: {
+          id: id
+        }
+      });
+      //console.log(message);
+    });
+    const json = JSON.stringify(obj);
+    res.json(json);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+const modifyEntry = async (req, res, next) => {
+  try {
+    await Message.update({ name: req.body.name, email: req.body.email, message: req.body.message }, {
+      where: {
+        id: req.body.id
+      }
+    });
+    res.json(JSON.stringify({ editSuccess: true }));
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+const createEntry = async (req, res, next) => {
+  try {
+    await Message.create({
+      name: req.body.name,
+      email: req.body.email,
+      message: req.body.message,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+  res.json(JSON.stringify({
+    addSuccess: true
+  }));
+}
+
 module.exports = {
+  handleFormSubmission,
+  getAllEntries,
+  deleteEntries,
+  modifyEntry,
   createEntry,
 };
--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const contactMailer = require('../middleware/contactMailer');
-const { createEntry } = require('../middleware/contactDb');
+const { handleFormSubmission, getAllEntries, deleteEntries, modifyEntry, createEntry } = require('../middleware/contactDb');
 require('dotenv').config();
 
 const router = express.Router();
 
+// Route used for site contact form submission.
 router.route('/contact')
-.post([contactMailer, createEntry], (req, res, next) => {
+.post([contactMailer, handleFormSubmission], (req, res, next) => {
   res.status(200).json({
     message: "Message recieved!",
     severity: 'success',
   });
-})
+});
+
+// Routes used for admin tool.
+router.route('/admin/contact/api')
+.get(getAllEntries)
+.delete(deleteEntries)
+.put(modifyEntry)
+.post(createEntry);
 
 module.exports = router;
